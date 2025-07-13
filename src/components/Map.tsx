cf33350a2d86954d1a64720efc56ee436a84d914
Richard Higgins <richhiggins1982@gmail.com>
2025-07-13 13:30:06 -0400

    Update Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,1 +1,7 @@
+<div className="flex-1 flex flex-col">
+  <div className="h-96 mb-6 bg-gray-100 flex items-center justify-center text-gray-500">
+    Map temporarily removed
+  </div>
+  <ResourceList resources={filteredResources} />
+</div>
 
